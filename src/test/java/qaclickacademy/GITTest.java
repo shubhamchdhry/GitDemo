ba047f9c599ed383688c4773eb3a9c0dd98ca749
shubham <shubham.chdhry1996@gmail.com>
2022-02-23 22:58:57 +0530

    this is commit from Asian time zone

---
+++ src/test/java/qaclickacademy/GITTest.java
@@ -0,0 +1,12 @@
+package qaclickacademy;
+
+import junit.framework.TestCase;
+
+public class GITTest extends TestCase {
+	private void syso() {
+		// TODO Auto-generated method stub
+		System.out.println("gittest");
+
+	}
+
+}
