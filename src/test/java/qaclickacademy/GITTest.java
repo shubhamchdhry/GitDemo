3cbc8f940a71c598c0492e42f9074b7eaa8033a8
shubham <shubham.chdhry1996@gmail.com>
2022-02-23 23:11:00 +0530

    American guy commit

--- src/test/java/qaclickacademy/GITTest.java
+++ src/test/java/qaclickacademy/GITTest.java
@@ -6,7 +6,10 @@ public class GITTest extends TestCase {
 	private void syso() {
 		// TODO Auto-generated method stub
 		System.out.println("gittest");
-
+		System.out.println("gittest1");
+		System.out.println("gittest2");
+		System.out.println("gittest3");
+		System.out.println("gittest4");
 	}
 
 }
